f66a63214807146158514768e7b834cde835bed8
Ahmed  Abdelhamed Morsy <119444757+ahmedmorsy396@users.noreply.github.com>
2024-10-03 17:45:15 +0300

    Add files via upload

---
+++ Max and min.py
@@ -0,0 +1,10 @@
+# This is a sample Python code determine min and max values
+arr = []
+for i in range(3):
+    Numbers = int(input('please enter the next number:'))
+    arr.append(Numbers)
+print('The max number is:', max(arr))
+print('The min number is:', min(arr))
+
+
+
