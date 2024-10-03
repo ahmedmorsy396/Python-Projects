0c586cd04d5bc4674d121941f8eae91b24da0c42
Ahmed  Abdelhamed Morsy <119444757+ahmedmorsy396@users.noreply.github.com>
2024-10-03 17:46:26 +0300

    Add files via upload

---
+++ Nagative_positive value.py
@@ -0,0 +1,11 @@
+# This is a sample Python code determine number positive or negative.
+
+Number = int(input('please enter the number to determine negative / positive: '))
+if Number > 0:
+    print('This number is positive')
+elif Number < 0:
+    print('This number is negative')
+else:
+    print('This nuber equal zero')
+
+
